5d65f67da777a4bd32ecde8adc7fc43034c82215
luxinli <luxl@hongyantu.net>
2019-04-04 11:19:03 +0800

    add

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -33,19 +33,46 @@
         </div>
     </div>
   </div> -->
-  <div>
-    <section>Todo</section>
+  <div class="container">
+    <ul class="tab-container">
+      <li class="active">全部</li>
+      <li>已完成</li>
+      <li>未完成</li>
+    </ul>
+    <ul class="todo-list">
+      <li>
+        <input type="checkbox">1111 <button class="del-button">删除</button>
+      </li>
+      <li>
+        <input type="checkbox">2222 <button class="del-button">删除</button>
+      </li>
+    </ul>
+    <!-- <mptoast /> -->
   </div>
 </template>
 
 <script>
+import mptoast from 'mptoast'
+
 export default {
   data () {
     return {
-      motto: 'Hello miniprograme'
+      motto: 'Hello miniprograme',
+      userInfo: {},
+      isLogin: false
     }
   },
+  components: {
+    mptoast
+  },
   methods: {
+    getUserInfo () {
+      console.log(111)
+    }
+    // showToast () {
+    //   console.log(111111111)
+    //   this.$mptoast('我是提示信息')
+    // }
   },
 
   created () {
@@ -53,6 +80,40 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.container {
+  padding: 20px;
+  font-size: 14px;
 
+  button {
+    font-size: 14px;
+    line-height: 1;
+    padding: 10px 15px;
+    vertical-align: bottom;
+  }
+
+  .tab-container {
+    font-size: 0;
+    > li {
+      display: inline-block;
+      font-size: 16px;
+      padding: 10px 15px;
+       border-bottom: 1px solid transparent;
+    }
+    > .active {
+      color: red;
+      border-bottom: 1px solid red;
+    }
+  }
+  .todo-list {
+    margin-top: 10px;
+    width: 100%;
+    > li {
+      margin-top: 10px;
+    }
+    .del-button {
+      display: inline-block;
+    }
+  }
+}
 </style>
