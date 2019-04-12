8c712337ce14b16d41df2ddf07523f38142550d6
daialihexiaoniu <397698632@qq.com>
2019-04-12 14:34:06 +0800

    change todo

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,25 +1,47 @@
 <template>
   <div class="container">
-    <!-- <ul class="tab-container">
-      <li class="active">全部</li>
-      <li>已完成</li>
-      <li>未完成</li>
-    </ul> -->
+    <ul class="tab-container">
+      <li :class="{active : status==2}" @click="changeStatus(2)">未完成</li>
+      <li :class="{active : status==1}" @click="changeStatus(1)">已完成</li>
+      <li :class="{active : status==0}" @click="changeStatus(0)">全部</li>
+    </ul>
     <div class="add-task">
       <input class="add-task-input" type="text" placeholder="添加todo" v-model="newTask">
       <button class="add-button" @click="addTask()">添加</button>
     </div>
-    <ul class="todo-list">
+    <ul class="todo-list" v-if="status === 0">
       <li v-for="(task, index) in tasks" :key="index">
-        <input type="checkbox" @click="completeTask(task)" :checked="task.complete">
-        <span :class="{'complete' : task.complete}">{{task.body}}</span>
-        <button @click="deleteTask(task)" class="del-button">删除</button>
+        <div class="todo-text-wrap">
+          <input type="checkbox" @click="completeTask(task)" :checked="task.complete">
+          <span :class="{'complete' : task.complete}">{{task.body}}</span>
+          <button @click="deleteTask(task)" class="del-button">删除</button>
+        </div>
       </li>
     </ul>
+    <ul class="todo-list" v-if="status === 1">
+      <li v-for="(task, index) in tasks" :key="index">
+        <div class="todo-text-wrap" v-if="task.complete">
+          <input type="checkbox" @click="completeTask(task)" :checked="task.complete">
+          <span :class="{'complete' : task.complete}">{{task.body}}</span>
+          <button @click="deleteTask(task)" class="del-button">删除</button>
+        </div>
+      </li>
+    </ul>
+    <ul class="todo-list" v-if="status === 2">
+      <li v-for="(task, index) in tasks" :key="index">
+        <div class="todo-text-wrap" v-if="!task.complete">
+          <input type="checkbox" @click="completeTask(task)" :checked="task.complete">
+          <span :class="{'complete' : task.complete}">{{task.body}}</span>
+          <button @click="deleteTask(task)" class="del-button">删除</button>
+        </div>
+      </li>
+    </ul>
+    <mptoast />
   </div>
 </template>
 
 <script>
+import mptoast from 'mptoast'
 export default {
   data () {
     return {
@@ -27,14 +49,17 @@ export default {
       userInfo: {},
       isLogin: false,
       newTask: '',
+      status: 2,
       tasks: [
         {body: '示例1', complete: false},
         {body: '示例2', complete: false}
       ]
     }
   },
+  components: {
+    mptoast
+  },
   onShow: function () {
-    console.log(111)
     var _this = this
     wx.getStorage({
       key: 'task',
@@ -44,6 +69,9 @@ export default {
     })
   },
   methods: {
+    changeStatus (status) {
+      this.status = status
+    },
     completeTask (task) {
       let index = this.tasks.indexOf(task)
       this.tasks[index].complete = !task.complete
@@ -60,7 +88,8 @@ export default {
       })
     },
     addTask () {
-      if (this.newTask === '') {
+      if (this.newTask.replace(/^\s+|\s+$/g, '') === '') {
+        this.$mptoast('请输入todo内容')
         return
       }
       this.tasks.push({body: this.newTask, complete: false})
@@ -92,11 +121,11 @@ export default {
       display: inline-block;
       font-size: 16px;
       padding: 10px 15px;
-       border-bottom: 1px solid transparent;
+      border-bottom: 1px solid transparent;
     }
     > .active {
-      color: red;
-      border-bottom: 1px solid red;
+      color: #fe3f3f;
+      border-bottom: 1px solid #fe3f3f;
     }
   }
   .add-task {
@@ -121,7 +150,7 @@ export default {
   .todo-list {
     margin-top: 10px;
     width: 100%;
-    > li {
+    .todo-text-wrap {
       padding: 5px 0;
       height: 34px;
       line-height: 34px;
